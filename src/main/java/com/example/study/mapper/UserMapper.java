df4f3f9d6e958c85590350174985aa99275f3c6e
ljj <422068923@qq.com>
2019-09-28 00:10:55 +0800

    w

---
+++ src/main/java/com/example/study/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.study.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public class UserMapper {
+}
---
+++ src/main/java/com/example/study/model/User.java
@@ -0,0 +1,10 @@
+package com.example.study.model;
+
+public class User {
+    private String id;
+    private String name;
+    private String accountId;
+    private String token;
+    private String gmtCreate;
+    private String gmnModified;
+}
